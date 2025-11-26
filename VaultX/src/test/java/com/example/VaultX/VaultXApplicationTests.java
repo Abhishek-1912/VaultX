479895f5264e0fa16d51f64aaaaa2fed87fbe686
Abhishek <you@example.com>
2025-11-26 18:20:48 +0530

    initial commit

---
+++ VaultX/src/test/java/com/example/VaultX/VaultXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VaultX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
