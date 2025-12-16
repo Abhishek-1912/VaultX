7da0ad6a2eef6bf4ccc10c01c28c33d8fdaaf541
Abhishek <you@example.com>
2025-12-16 11:24:34 +0530

    Initial commit - Spring Boot project

---
+++ src/main/java/com/example/ValutX/ValutXApplication.java
@@ -0,0 +1,13 @@
+package com.example.ValutX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValutXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValutXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ValutX/ValutXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ValutX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValutXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
